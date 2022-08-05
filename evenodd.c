8d9fb7e14403f7d3ca7dc6db03f846687a2789a7
Apu kumar Giri <87590236+apukumar212@users.noreply.github.com>
2022-08-05 19:31:27 +0530

    Add files via upload

---
+++ evenodd.c
@@ -0,0 +1,12 @@
+ #include<stdio.h>
+ #include<stdlib.h>
+ int main()
+ {
+    int n;
+    printf("Enter any number: ");
+    scanf("%d",&n);
+    if(n%2==0) puts("even number");
+    else puts("odd number");
+    return 0;
+
+ }
